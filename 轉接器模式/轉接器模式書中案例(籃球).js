fd3e2477d03b36d1933fbff53be26ce849720c87
Jacky <Jj0932175456@gmail.com>
2023-03-28 11:34:58 +0800

    修正轉接器模式範例(程式碼位置調整)

--- 轉接器模式/轉接器模式書中案例(籃球).js
+++ 轉接器模式/轉接器模式書中案例(籃球).js
@@ -1,83 +1,80 @@
 class Player {
-    constructor(name){
-        this.name = name
+    constructor(name) {
+        this.name = name;
     }
-    Attack(){}
-    Defense(){}
+    Attack() {}
+    Defense() {}
 }
 
 // 前、中、後峰
 class Forwards extends Player {
-    constructor(name){
-        super(name)
+    constructor(name) {
+        super(name);
     }
-    Attack(){
-        console.log(`前鋒${this.name}進攻`)
+    Attack() {
+        console.log(`前鋒${this.name}進攻`);
     }
-    Defense(){
-        console.log(`前鋒${this.name}防守`)
+    Defense() {
+        console.log(`前鋒${this.name}防守`);
     }
-} 
+}
 class Center extends Player {
-    constructor(){
-        super()
+    constructor() {
+        super();
     }
-    Attack(){
-        console.log(`中鋒${this.name}進攻`)
+    Attack() {
+        console.log(`中鋒${this.name}進攻`);
     }
-    Defense(){
-        console.log(`中鋒${this.name}防守`)
+    Defense() {
+        console.log(`中鋒${this.name}防守`);
     }
-} 
+}
 
 class Guards extends Player {
-    constructor(){
-        super()
+    constructor() {
+        super();
     }
-    Attack(){
-        console.log(`後鋒${this.name}進攻`)
+    Attack() {
+        console.log(`後鋒${this.name}進攻`);
     }
-    Defense(){
-        console.log(`後鋒${this.name}防守`)
+    Defense() {
+        console.log(`後鋒${this.name}防守`);
     }
-} 
+}
 
 // 外籍中鋒
 class ForeignCenter {
-    constructor(name = 'no body'){
-        this.name = name
+    constructor(name = "no body") {
+        this.name = name;
     }
-    進攻(){
-        console.log(`外籍中鋒${this.name}進攻`)
+    進攻() {
+        console.log(`外籍中鋒${this.name}進攻`);
     }
-    防守(){
-        console.log(`外籍中鋒${this.name}防守`)
+    防守() {
+        console.log(`外籍中鋒${this.name}防守`);
     }
 }
 
 // 翻譯者
-class Translator extends Player{
-    constructor(name){
-        super(name)
-        this.ForeignCenterPlayer = new ForeignCenter()
-        this.ForeignCenterPlayer.name = name
-
-       
+class Translator extends Player {
+    constructor(name) {
+        super(name);
+        this.ForeignCenterPlayer = new ForeignCenter();
+        this.ForeignCenterPlayer.name = name;
     }
-    Attack(){
-        this.ForeignCenterPlayer.進攻()
+    Attack() {
+        this.ForeignCenterPlayer.進攻();
     }
-    Defense(){
-        this.ForeignCenterPlayer.防守()
+    Defense() {
+        this.ForeignCenterPlayer.防守();
     }
 }
 
-let b = new Forwards("巴蒂爾")
-b.Attack()
+let b = new Forwards("巴蒂爾");
+let m = new Forwards("麥克爾雷迪");
+let ym = new Translator("姚明");
 
-let m = new Forwards("麥克爾雷迪")
-m.Attack()
-
-let ym = new Translator('姚明')
-ym.Attack()
-ym.Defense()
+b.Attack();
+m.Attack();
+ym.Attack();
+ym.Defense();
