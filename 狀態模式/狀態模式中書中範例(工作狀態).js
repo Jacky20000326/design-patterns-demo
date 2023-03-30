7df8b1d5cc4611cda373bd1434b3d47f8f0b4d65
Jacky <Jj0932175456@gmail.com>
2023-03-30 17:17:10 +0800

    狀態模式範例新增

--- 狀態模式/狀態模式中書中範例(工作狀態).js
+++ 狀態模式/狀態模式中書中範例(工作狀態).js
@@ -1,104 +1,71 @@
 class State {
-    WriteProgram(w){}
+    // 顯示的數值
+    tempToDisplay(temperture) {}
+    vibToDisplay(vibration) {}
+    // 儲存的數值
+    tempToSave(temperture) {}
+    vibToSave(vibration) {}
 }
-// 早
-class MorningWorkState extends State{
-    WriteProgram(w){
-        if(w.Hour < 12){
-            console.log(`當前時間${w.Hour}點上午工作`)
-        }else{
-            w.SetState(new middayWorkState())
-            w.WriteProgram()
-        }
+
+// 公制的State
+class Metric extends State {
+    // 顯示的數值
+    tempToDisplay(temperture) {
+        console.log("顯示公制");
+        return temperture;
     }
-}
-//中
-class middayWorkState extends State{
-    WriteProgram(w){
-        if(w.Hour < 13){
-            console.log(`當前時間${w.Hour}點中午工作`)
-        }else{
-            w.SetState(new AfternoonWorkState())
-            w.WriteProgram()
-        }
+    vibToDisplay(vibration) {
+        console.log("顯示公制");
+        return vibration;
     }
-}
-// 下午
-class AfternoonWorkState extends State{
-    WriteProgram(w){
-        if(w.Hour < 17){
-            console.log(`當前時間${w.Hour}點中午工作`)
-        }else{
-            w.SetState(new NightWorkState())
-            w.WriteProgram()
-        }
+    // 儲存的數值
+    tempToSave(temperture) {
+        console.log("儲存公制");
     }
-}
-// 晚
-class NightWorkState extends State{
-   
-    WriteProgram(w){
-        if(w.TaskFinished){
-            w.SetState(new RestState())
-            w.WriteProgram()
-        }else{
-            if(w.Hour < 21){
-                console.log(`當前時間${w.Hour}點晚上工作`)
-            }else{
-                w.SetState(new SleepState())
-                w.WriteProgram()
-            }
-        }
+    vibToSave(vibration) {
+        console.log("儲存公制");
     }
 }
 
-// 睡眠
-class SleepState extends State{
-    WriteProgram(w){
-        console.log(`當前時間${w.Hour}要睡了`)
+// 英制的State
+class British extends State {
+    // 顯示的數值
+    tempToDisplay(temperture) {
+        console.log("顯示英制除二就好");
+        console.log(temperture);
+        return temperture / 2;
+    }
+    vibToDisplay(vibration) {
+        console.log("顯示英制除三就好");
+        return temperture / 3;
+    }
+    // 儲存的數值
+    tempToSave(temperture) {
+        console.log("儲存英制");
+    }
+    vibToSave(vibration) {
+        console.log("儲存英制");
     }
 }
 
-// 下班休息
-class RestState extends State{
-    WriteProgram(w){
-        console.log(`當前時間${w.Hour}下班了`)
+class MetricSystem {
+    SetState(state) {
+        this.state = state;
+    }
+    tempView(temp) {
+        this.state.tempToDisplay(temp);
+    }
+    vibView(vib) {
+        this.state.vibToDisplay(vib);
+    }
+    tempSave(temp) {
+        this.state.tempToSave();
+    }
+    vibSave() {
+        this.state.vibToSave();
     }
 }
 
-// 工作
-class Work {
-
-    constructor(){
-        this.current = new MorningWorkState()
-        this.Hour = null
-        this.TaskFinished = false
-    }
-
-    SetState(state){
-        this.current = state
-    }
-    WriteProgram(){
-        this.current.WriteProgram(this)
-    }
-}
-
-// 用戶端
-let emergencyProjects = new Work()
-emergencyProjects.Hour = 9
-emergencyProjects.WriteProgram()
-emergencyProjects.Hour = 10
-emergencyProjects.WriteProgram()
-emergencyProjects.Hour = 12
-emergencyProjects.WriteProgram()
-emergencyProjects.Hour = 13
-emergencyProjects.WriteProgram()
-emergencyProjects.Hour = 14
-emergencyProjects.WriteProgram()
-emergencyProjects.Hour = 17
-emergencyProjects.WriteProgram()
-
-emergencyProjects.TaskFinished = true
-emergencyProjects.WriteProgram()
-emergencyProjects.Hour = 19
-emergencyProjects.WriteProgram()
+let metricSystem = new MetricSystem();
+metricSystem.SetState(new British());
+console.log(metricSystem.tempView(30));
